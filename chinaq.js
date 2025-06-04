4f594ad201a2d9f3d404274f913b34866cd73ca5
ggl-wolfy <58021228+ggl-wolfy@users.noreply.github.com>
2025-06-04 15:11:18 +0800

    Update chinaq.js

--- chinaq.js
+++ chinaq.js
@@ -1,5 +1,4 @@
 // cSpell:words chinaq qplays
-const chinaqBaseUrl = "https://chinaq.fun";
 
 const REGEX = {
   detailsAirDate: /【首播】(\d{4}-\d{2}-\d{2})/,
@@ -11,8 +10,7 @@ const REGEX = {
   searchItemTitle: /<h1>([\s\S]*?) (?:ChinaQ)?線上看/,
 };
 
-async function getHostWithProtocol(url) {
-  // Match the protocol and host part of the URL
+function getHostWithProtocol(url) {
   const match = url.match(/^(https:\/\/(?:[^\/]+\.)?chinaq[^\/]+)/i);
   if (match) return match[1]
   throw new Error(`Invalid ChinaQ URL: ${url}`)
@@ -32,23 +30,21 @@ function extractMatches(html, regex) {
   return result?.[1]?.trim() ?? null;
 }
 
-async function searchResults(keyword) {
+async function searchResults(url) {
   try {
-    const html = await fetchHtml(keyword);
-    const href = keyword;
+    const html = await fetchHtml(url);
     const title = extractMatches(html, REGEX.searchItemTitle);
     const image = extractMatches(html, REGEX.searchItemImg);
 
     const results = [];
-    if (href && title && image) {
-      const baseUrl = getHostWithProtocol(keyword);
-      results.push({ title, image: baseUrl + image, href });
+    if (url && title && image) {
+      const baseUrl = getHostWithProtocol(url);
+      results.push({ title, image: baseUrl + image, href: url });
     }
 
-    // console.log(JSON.stringify(results));
     return JSON.stringify(results);
   } catch (error) {
-    console.log(`Failed to extract series from URL: ${keyword}`);
+    console.log(`Failed to extract series from URL: ${url}`);
     return JSON.stringify([]);
   }
 }
@@ -60,7 +56,6 @@ async function extractDetails(url) {
     const airdate = extractMatches(html, REGEX.detailsAirDate) || 'Aired: Unknown';
     const details = [{ description, alias: 'N/A', airdate }];
 
-    console.log(JSON.stringify(details));
     return JSON.stringify(details);
   } catch (error) {
     console.log('Failed to extract details:', error);
@@ -76,24 +71,26 @@ async function extractEpisodes(url) {
     const sourcesMatch = html.match(REGEX.episodeSources);
     if (!sourcesMatch) throw new Error('Failed to extract source');
 
+    const baseUrl = getHostWithProtocol(url);
+
     const sourcesHtml = sourcesMatch[1];
     const episodeSource = sourcesHtml.matchAll(REGEX.episodePage);
     for (const [_, pageUrl, title] of episodeSource) {
       const episodeNum = extractMatches(title, REGEX.episodeNum) ?? '0'
-      const number = parseInt(episodeNum, 10);
-
-      if (number === 0) {
+      if (episodeNum === '0') {
         console.log(`Skipped episode: [${title}]`);
         continue;
       }
 
-      // https://chinaq.fun/tv-cn/202557998/ep1.html ==> https://chinaq.fun/qplays/202557998/ep1
       const href = pageUrl.replace(/^\/[^/]+/,'/qplays').replace('.html','');
 
-      episodes.unshift({ href, number, title });
+      episodes.unshift({
+        href: baseUrl + href,
+        number: parseInt(episodeNum, 10),
+        title
+      });
     }
 
-    console.log(episodes);
     return JSON.stringify(episodes);
   } catch (error) {
     console.log('Failed to extract episodes:', error);
@@ -102,14 +99,11 @@ async function extractEpisodes(url) {
 }
 
 async function extractStreamUrl(url) {
-  console.log(`Stream URL: [${url}]`);
-
   try {
     const html = await fetchHtml(url);
     const sources = JSON.parse(html)?.video_plays;
     const result = { streams: sources.map(source => source.play_data) };
 
-    console.log("Result:", result);
     return JSON.stringify(result);
   } catch (error) {
     console.log('Failed to extract streams:', error);
